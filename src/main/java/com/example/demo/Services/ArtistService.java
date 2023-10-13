e512b37d4c1a9a71f3e1eb173f34cbf4403809c1
osm17 <osman.sheikh@edu.edugrade.se>
2023-10-13 08:01:40 +0100

    .

--- src/main/java/com/example/demo/Services/ArtistService.java
+++ src/main/java/com/example/demo/Services/ArtistService.java
@@ -1,4 +1,46 @@
 package com.example.demo.Services;
 
-public class ArtistService {
+import com.example.demo.Entity.Artist;
+import com.example.demo.Repositories.ArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ArtistService implements ArtistServiceInterface {
+
+    @Autowired
+    private ArtistRepository artistRepository;
+
+    @Override
+    public List<Artist> getAllArtists() {
+        return artistRepository.findAll();
+    }
+
+    @Override
+    public Artist addArtist(Artist artist) {
+        return artistRepository.save(artist);
+    }
+
+    @Override
+    public Artist updateArtist(Artist artist) {
+
+        return artistRepository.save(artist);
+    }
+
+    @Override
+    public void deleteArtist(int id) {
+        artistRepository.deleteById(id);
+    }
+
+    @Override
+    public Artist getArtistById(int id) {
+        Optional<Artist> artist = artistRepository.findById(id);
+        if(artist.isPresent()) {
+            return artist.get();
+        }
+        return null;  // exception here in the future
+    }
 }
--- src/main/java/com/example/demo/Services/ArtistServiceInterface.java
+++ src/main/java/com/example/demo/Services/ArtistServiceInterface.java
@@ -1,4 +1,22 @@
 package com.example.demo.Services;
 
+import com.example.demo.Entity.Artist;
+import java.util.List;
+
 public interface ArtistServiceInterface {
+
+
+    List<Artist> getAllArtists();
+
+
+    Artist addArtist(Artist artist);
+
+
+    Artist updateArtist(Artist artist);
+
+
+    void deleteArtist(int id);
+
+
+    Artist getArtistById(int id);
 }
